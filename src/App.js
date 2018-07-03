4864243711e4b3a35c9a2b74775d6be8766cab2d
AlishaFernandes <38370747+AlishaFernandes@users.noreply.github.com>
2018-07-03 12:12:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://barcampbangalore.com/bcb/wp-content/uploads/2018/01/BCB-LOGO-open-graph.jpg" className="App-logo" alt="logo" />
+          <p><a href="http://barcampbangalore.com/bcb" title="Barcamp Bangalore" tabindex="-1">Barcamp Bangalore</a></p>
+
+        </header>
+        <p className="App-intro">
+
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
